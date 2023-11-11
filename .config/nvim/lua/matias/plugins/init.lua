86a198d2d2716e407d00b920e42055e21d0fb796
André Matias <andrematias@macpro.local>
2023-11-11 01:50:01 -0300

    Add new plugins
    Themes:
      Gruvbox
      modus-theme
    
    Behave
      vim-bbye

--- .config/nvim/lua/matias/plugins/init.lua
+++ .config/nvim/lua/matias/plugins/init.lua
@@ -1,7 +1,8 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
-
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
-
   "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
+  "ellisonleao/gruvbox.nvim",
+  "ishan9299/modus-theme-vim",
+  "moll/vim-bbye",
 }
