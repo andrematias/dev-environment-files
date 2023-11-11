9faa997c7a4ba554504f78d7b8b08aea2255669e
André Matias <andrematias@macpro.local>
2023-11-11 13:41:10 -0300

    Configurando novas defaults options

--- .config/nvim/lua/matias/core/options.lua
+++ .config/nvim/lua/matias/core/options.lua
@@ -1,12 +1,13 @@
 local opt = vim.opt -- for conciseness
+local g = vim.g -- Global variables
 
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.tabstop = 4 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 4 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
@@ -38,5 +39,55 @@ opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
--- turn off swapfile
-opt.swapfile = false
+opt.swapfile = false -- turn off swapfile
+opt.cmdheight = 0 -- hide command line input
+opt.showbreak = " 󰌑 " -- show a icon in lines breaked
+opt.textwidth = 100 -- set line width to 100 characters
+opt.showmatch = true --highlight matching parenthesis
+opt.colorcolumn = "80" -- Line lenght marker at 80 columns
+opt.splitright = true -- Vertical split to the right
+opt.splitbelow = true -- Horizontal split to the bottom
+opt.linebreak = true -- Wrap on word boundary
+opt.mouse = "a" -- Enable mouse support
+opt.hidden = true -- Enable background buffers
+opt.history = 100 -- Remember N lines in history
+opt.lazyredraw = true -- Faster scrolling
+opt.synmaxcol = 240 -- Max column for syntax highlight
+opt.updatetime = 250 -- ms to wait for trigger an event
+opt.mouse = "a" -- Enable mouse support
+
+-- Disable nvim intro
+opt.shortmess:append("sI")
+
+-- -- Disable builtin plugins
+local disabled_built_ins = {
+  "2html_plugin",
+  "getscript",
+  "getscriptPlugin",
+  "gzip",
+  "logipat",
+  "netrw",
+  "netrwPlugin",
+  "netrwSettings",
+  "netrwFileHandlers",
+  "matchit",
+  "tar",
+  "tarPlugin",
+  "rrhelper",
+  "spellfile_plugin",
+  "vimball",
+  "vimballPlugin",
+  "zip",
+  "zipPlugin",
+  "tutor",
+  "rplugin",
+  "synmenu",
+  "optwin",
+  "compiler",
+  "bugreport",
+  "ftplugin",
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+  g["loaded_" .. plugin] = 1
+end
