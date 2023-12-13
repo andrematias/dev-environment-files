6ea630f7d82f03e02ca97d91ebfc2f758dec2b80
André Matias <matias@archlinux.local>
2023-12-13 11:28:37 -0300

    Customiza a lualine

--- .config/nvim/lua/matias/plugins/lualine.lua
+++ .config/nvim/lua/matias/plugins/lualine.lua
@@ -1,22 +1,97 @@
+-- stylua: ignore
+local colors = {
+  bg       = '#252525',
+  fg       = '#bbc2cf',
+  yellow   = '#ECBE7B',
+  cyan     = '#008080',
+  darkblue = '#081633',
+  green    = '#98be65',
+  orange   = '#FF8800',
+  violet   = '#a9a1e1',
+  magenta  = '#c678dd',
+  blue     = '#51afef',
+  red      = '#ec5f67',
+}
+
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local lualine = require("lualine")
+    -- lualine.setup(config)
+
     local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
     -- configure lualine with modified theme
     lualine.setup({
       options = {
-        -- theme = "darkplus",
+        -- theme = "gruvbox",
+        theme = {
+          normal = {
+            a = { fg = colors.fg, bg = colors.bg, gui = "bold" },
+            b = { fg = colors.fg, bg = colors.bg },
+            c = { fg = colors.fg, bg = colors.bg },
+          },
+          inactive = {
+            a = { fg = colors.fg, bg = colors.bg, gui = "bold" },
+            b = { fg = colors.fg, bg = colors.bg },
+            c = { fg = colors.fg, bg = colors.bg },
+          },
+          command = {
+            a = { fg = colors.fg, bg = colors.bg, gui = "bold" },
+            b = { fg = colors.fg, bg = colors.bg },
+            c = { fg = colors.fg, bg = colors.bg },
+          },
+          terminal = {
+            a = { fg = colors.fg, bg = colors.bg, gui = "bold" },
+            b = { fg = colors.fg, bg = colors.bg },
+            c = { fg = colors.fg, bg = colors.bg },
+          },
+        },
+        globalstatus = true,
         component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
-        disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
-        always_divide_middle = true,
+        disabled_filetypes = { "alpha", "dashboard", "Outline" },
+        always_divide_middle = false,
       },
       sections = {
+        lualine_a = {
+          {
+            function()
+              return ""
+            end,
+            color = function()
+              -- auto change color according to neovims mode
+              local mode_color = {
+                n = colors.red,
+                i = colors.green,
+                v = colors.blue,
+                [""] = colors.blue,
+                V = colors.blue,
+                c = colors.magenta,
+                no = colors.red,
+                s = colors.orange,
+                S = colors.orange,
+                [""] = colors.orange,
+                ic = colors.yellow,
+                R = colors.violet,
+                Rv = colors.violet,
+                cv = colors.red,
+                ce = colors.red,
+                r = colors.cyan,
+                rm = colors.cyan,
+                ["r?"] = colors.cyan,
+                ["!"] = colors.red,
+                t = colors.red,
+              }
+              return { bg = colors.bg, fg = mode_color[vim.fn.mode()] }
+            end,
+            padding = { left = 1, right = 1 },
+          },
+        },
         lualine_c = { "filename", "filesize", "%{PencilMode()}" },
         lualine_x = {
+          { "searchcount" },
           {
             lazy_status.updates,
             cond = lazy_status.has_updates,
@@ -27,7 +102,7 @@ return {
           { "filetype" },
         },
       },
-      extensions = { "nvim-tree" },
+      extensions = { "nvim-tree", "aerial", "mason" },
     })
   end,
 }
