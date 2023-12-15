f73b1eb53d7e5ff0796efb4ec1f174a314384975
André Matias <matias@archlinux.local>
2023-12-15 08:54:56 -0300

    Adiciona as cores do tema onedark na conf do lualine

--- .config/nvim/lua/matias/plugins/lualine.lua
+++ .config/nvim/lua/matias/plugins/lualine.lua
@@ -1,17 +1,5 @@
 -- stylua: ignore
-local colors = {
-  bg       = '#252525',
-  fg       = '#bbc2cf',
-  yellow   = '#ECBE7B',
-  cyan     = '#008080',
-  darkblue = '#081633',
-  green    = '#98be65',
-  orange   = '#FF8800',
-  violet   = '#a9a1e1',
-  magenta  = '#c678dd',
-  blue     = '#51afef',
-  red      = '#ec5f67',
-}
+local colors = require('onedark.palette').darker
 
 return {
   "nvim-lualine/lualine.nvim",
