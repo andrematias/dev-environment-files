7d0e02fdfc4e142733700c8e2f980e1d7213ab21
André Matias <matias@archlinux.local>
2023-12-14 21:10:02 -0300

    Remove o guibg nos highlight do Telescope

--- .config/nvim/lua/matias/plugins/telescope.lua
+++ .config/nvim/lua/matias/plugins/telescope.lua
@@ -40,5 +40,12 @@ return {
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+
+    vim.cmd([[
+      highlight Float guibg=none
+      highlight FloatBorder guibg=none
+      highlight TelescopeTitle guibg=none
+      highlight TelescopeNormal guibg=none
+    ]])
   end,
 }
