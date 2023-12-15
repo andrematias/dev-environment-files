1120245c02c2c90139e7eacded7d0124e7a40f1f
André Matias <andrematias@macpro.local>
2023-12-15 10:17:42 -0300

    Adiciona atalho no Alpha Dashboard para editar as configurações

--- .config/nvim/lua/matias/plugins/alpha-nvim.lua
+++ .config/nvim/lua/matias/plugins/alpha-nvim.lua
@@ -24,6 +24,7 @@ return {
       dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
       dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
       dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+      dashboard.button("c", " > Edit Configurations", "<cmd>e $HOME/.config/nvim/init.lua<CR>"),
       dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
     }
 
