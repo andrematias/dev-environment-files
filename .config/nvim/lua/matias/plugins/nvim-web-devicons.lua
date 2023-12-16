7772a9d08303c501f3b12e4428a8e187072ca6f5
André Matias <andrematias@macpro.local>
2023-12-15 23:04:08 -0300

    Adiciona icone alternativo para arquivos do tipo Toml

--- .config/nvim/lua/matias/plugins/nvim-web-devicons.lua
+++ .config/nvim/lua/matias/plugins/nvim-web-devicons.lua
@@ -2,6 +2,12 @@ return {
   "nvim-tree/nvim-web-devicons",
   config = function()
     require("nvim-web-devicons").set_icon({
+      toml = {
+        icon = "",
+        color = "#e535ab",
+        cterm_color = "199",
+        name = "Toml",
+      },
       gql = {
         icon = "",
         color = "#e535ab",
