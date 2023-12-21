f7c6c8e77bf7dc97146a4ba557ddcd49667c2890
André Matias <matias@archlinux.local>
2023-12-21 01:11:00 -0300

    Configura debuggers cppdbg e codelldb no dap

--- .config/nvim/lua/matias/plugins/dap/mason-nvim-dap.lua
+++ .config/nvim/lua/matias/plugins/dap/mason-nvim-dap.lua
@@ -13,6 +13,51 @@ return {
           -- Keep original functionality
           require("mason-nvim-dap").default_setup(config)
         end,
+        cppdbg = function(set)
+          set.configurations = {
+            {
+              name = "CPPDBG: Relative Workspace",
+              type = "cppdbg",
+              request = "launch",
+              runInTerminal = true,
+              program = function()
+                return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+              end,
+              cwd = function()
+                return vim.fn.input("Workspace: ", vim.fn.getcwd() .. "/", "file")
+              end,
+              stopOnEntry = true,
+              args = function()
+                local input_args = vim.fn.input("Args to executable: ")
+                return vim.split(input_args, " ")
+              end,
+            },
+          }
+          require("mason-nvim-dap").default_setup(set) -- don't forget this!
+        end,
+
+        codelldb = function(set)
+          set.configurations = {
+            {
+              name = "LLDB: Relative",
+              type = "codelldb",
+              request = "launch",
+              program = function()
+                return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+              end,
+              cwd = function()
+                return vim.fn.input("Workspace: ", vim.fn.getcwd() .. "/", "file")
+              end,
+              stopOnEntry = true,
+              runInTerminal = true,
+              args = function()
+                local input_args = vim.fn.input("Args to executable: ")
+                return vim.split(input_args, " ")
+              end,
+            },
+          }
+          require("mason-nvim-dap").default_setup(set) -- don't forget this!
+        end,
       },
     })
   end,
