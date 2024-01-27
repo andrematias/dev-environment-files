60930eeabe610d5f3931abe6ac84e32466fa4dd4
André Matias <andre.matias@mutant.com.br>
2024-01-27 02:00:21 -0300

    Configura o alpha nvim

--- .config/nvim/lua/matias/plugins/alpha-nvim.lua
+++ .config/nvim/lua/matias/plugins/alpha-nvim.lua
@@ -14,6 +14,10 @@ return {
       [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
       [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
       [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+      [[                                                 ]],
+      [[                     --                          ]],
+      [[    Faça o seu melhor hoje, Será o suficiente.   ]],
+      [[                                                 ]],
     }
 
     -- Set menu
@@ -23,7 +27,6 @@ return {
       dashboard.button("SPC ee", "  Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
       dashboard.button("SPC ff", "󰱼  Find File", "<cmd>Telescope find_files<CR>"),
       dashboard.button("SPC fs", "  Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("SPC wr", "󰁯  Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
       dashboard.button("c", "  Edit Configurations", "<cmd>e $HOME/.config/nvim/init.lua<CR>"),
       dashboard.button("q", "  Quit NVIM", "<cmd>qa<CR>"),
     }
