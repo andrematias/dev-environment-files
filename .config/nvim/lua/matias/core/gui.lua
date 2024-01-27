94b16ae8f402029e71783f74e5da05009b285d72
André Matias <andrematias@macpro.local>
2024-01-27 12:40:08 -0300

    Configuracoes para neovide no macos

--- .config/nvim/lua/matias/core/gui.lua
+++ .config/nvim/lua/matias/core/gui.lua
@@ -1,56 +1,61 @@
 if vim.g.neovide then
-
   local function rpcrequest(method, ...)
-      return vim.rpcrequest(vim.g.neovide_channel_id, method, ...)
+    return vim.rpcrequest(vim.g.neovide_channel_id, method, ...)
   end
 
   local function set_clipboard(register)
-      return function(lines, regtype)
-          rpcrequest("neovide.set_clipboard", lines)
-      end
+    return function(lines, regtype)
+      rpcrequest("neovide.set_clipboard", lines)
+    end
   end
 
   local function get_clipboard(register)
-      return function()
-          return rpcrequest("neovide.get_clipboard", register)
-      end
+    return function()
+      return rpcrequest("neovide.get_clipboard", register)
+    end
   end
 
   vim.g.clipboard = {
-      name = "neovide",
-      copy = {
-          ["+"] = set_clipboard("+"),
-          ["*"] = set_clipboard("*"),
-      },
-      paste = {
-          ["+"] = get_clipboard("+"),
-          ["*"] = get_clipboard("*"),
-      },
-      cache_enabled = 0
+    name = "neovide",
+    copy = {
+      ["+"] = set_clipboard("+"),
+      ["*"] = set_clipboard("*"),
+    },
+    paste = {
+      ["+"] = get_clipboard("+"),
+      ["*"] = get_clipboard("*"),
+    },
+    cache_enabled = 0,
   }
 
   vim.api.nvim_create_autocmd({ "VimLeavePre" }, {
-      pattern = "*",
-      once = true,
-      nested = true,
-      callback = function()
-          rpcrequest("neovide.quit", vim.v.exiting)
-      end
+    pattern = "*",
+    once = true,
+    nested = true,
+    callback = function()
+      rpcrequest("neovide.quit", vim.v.exiting)
+    end,
   })
 
-  vim.opt.guifont = "IosevkaTerm Nerd Font:h12"
+  if vim.loop.os_uname().sysname == "Darwin" then
+    vim.opt.guifont = "IosevkaTerm Nerd Font:h16"
+    vim.g.neovide_refresh_rate = 60
+  else
+    vim.opt.guifont = "IosevkaTerm Nerd Font:h12"
+    vim.g.neovide_refresh_rate = 144
+  end
+
   vim.opt.linespace = 1
   vim.g.neovide_cursor_animation_length = 0.10
   vim.g.neovide_cursor_trail_size = 0.5
   vim.g.neovide_confirm_quit = true
-  vim.g.neovide_theme = 'auto'
+  vim.g.neovide_theme = "auto"
   vim.g.neovide_padding_top = 0
   vim.g.neovide_padding_bottom = 0
   vim.g.neovide_padding_right = 0
   vim.g.neovide_padding_left = 0
   vim.g.neovide_scroll_animation_length = 0.5
   vim.g.neovide_scroll_animation_far_lines = 4
-  vim.g.neovide_refresh_rate = 144
   vim.g.neovide_cursor_antialiasing = true
   vim.g.neovide_cursor_vfx_mode = "ripple"
   -- vim.g.neovide_cursor_vfx_mode = "railgun"
