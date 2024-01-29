ecec1ab6ef0a90749e7d948e53b7d20cf37b1839
André Matias <andre.matias@mutant.com.br>
2024-01-28 21:37:19 -0300

    Menos icones no telescope

--- .config/nvim/lua/matias/plugins/telescope.lua
+++ .config/nvim/lua/matias/plugins/telescope.lua
@@ -14,7 +14,7 @@ return {
     telescope.setup({
       defaults = {
         prompt_prefix = " ",
-        selection_caret = " ",
+        selection_caret = ">",
         path_display = { "smart" },
         mappings = {
           i = {
