a0659e220035eb349adc073069bdc66b8f008fba
André Matias <andre.matias@mutant.com.br>
2024-01-29 15:10:50 -0300

    Configura telescope

--- .config/nvim/lua/matias/plugins/telescope.lua
+++ .config/nvim/lua/matias/plugins/telescope.lua
@@ -1,6 +1,5 @@
 return {
   "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
@@ -15,7 +14,7 @@ return {
       defaults = {
         prompt_prefix = "  ",
         selection_caret = "> ",
-        path_display = { "smart" },
+        path_display = { "absolute" },
         mappings = {
           i = {
             ["<C-n>"] = actions.cycle_history_next,
@@ -37,8 +36,8 @@ return {
             ["<C-u>"] = actions.preview_scrolling_up,
             ["<C-d>"] = actions.preview_scrolling_down,
 
-            ["<PageUp>"] = actions.results_scrolling_up,
-            ["<PageDown>"] = actions.results_scrolling_down,
+            ["<C-S-k>"] = actions.results_scrolling_up,
+            ["<C-S-j>"] = actions.results_scrolling_down,
 
             ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
             ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
@@ -46,85 +45,33 @@ return {
             ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
             ["<C-l>"] = actions.complete_tag,
             ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
-          },
-
-          n = {
             ["<esc>"] = actions.close,
-            ["<CR>"] = actions.select_default,
-            ["<C-x>"] = actions.select_horizontal,
-            ["<C-v>"] = actions.select_vertical,
-            ["<C-t>"] = actions.select_tab,
-
-            ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-            ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-            ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-            ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-
-            ["j"] = actions.move_selection_next,
-            ["k"] = actions.move_selection_previous,
-            ["H"] = actions.move_to_top,
-            ["M"] = actions.move_to_middle,
-            ["L"] = actions.move_to_bottom,
-
-            ["<Down>"] = actions.move_selection_next,
-            ["<Up>"] = actions.move_selection_previous,
-            ["gg"] = actions.move_to_top,
-            ["G"] = actions.move_to_bottom,
-
-            ["<C-u>"] = actions.preview_scrolling_up,
-            ["<C-d>"] = actions.preview_scrolling_down,
-
-            ["<PageUp>"] = actions.results_scrolling_up,
-            ["<PageDown>"] = actions.results_scrolling_down,
-
-            ["?"] = actions.which_key,
           },
         },
       },
       pickers = {
-        oldfiles = {
-          previewer = false,
-          theme = "dropdown",
-        },
-        buffers = {
-          previewer = false,
-          theme = "dropdown",
-        },
-        live_grep = {
-          previewer = false,
-          theme = "dropdown",
-        },
-        projects = {
-          previewer = false,
-          theme = "dropdown",
-        },
         find_files = {
-          theme = "dropdown",
-          previewer = false,
           find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
         },
       },
     })
 
-    telescope.load_extension("fzf")
     telescope.load_extension("projects")
-    telescope.load_extension("aerial")
-    -- telescope.load_extension("notify")
 
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Fuzzy find files buffers" })
-
-    vim.cmd([[
-      highlight Float guibg=none
-      highlight FloatBorder guibg=none
-      highlight TelescopeTitle guibg=none
-      highlight TelescopeNormal guibg=none
-    ]])
+    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files theme=dropdown<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles theme=dropdown<cr>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep theme=dropdown<cr>", { desc = "Find string in cwd" })
+    keymap.set(
+      "n",
+      "<leader>fc",
+      "<cmd>Telescope grep_string theme=dropdown<cr>",
+      { desc = "Find string under cursor in cwd" }
+    )
+    keymap.set("n", "<leader>fb", "<cmd>Telescope buffers theme=dropdown<cr>", { desc = "Fuzzy find files buffers" })
+    keymap.set("n", "<leader>ft", "<cmd>Telescope treesitter theme=dropdown<cr>", { desc = "Fuzzy find symbols" })
+    keymap.set("n", "<leader>fp", "<cmd>Telescope projects theme=dropdown<cr>", { desc = "Fuzzy find projects" })
   end,
 }
