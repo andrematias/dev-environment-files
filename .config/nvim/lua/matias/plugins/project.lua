83f64e7196166c4ad06b89a8bdef9686d87e3d96
André Matias <andre.matias@mutant.com.br>
2024-01-29 15:11:09 -0300

    Remove keymap para projetos

--- .config/nvim/lua/matias/plugins/project.lua
+++ .config/nvim/lua/matias/plugins/project.lua
@@ -13,9 +13,5 @@ return {
       silent_chdir = false,
       datapath = vim.fn.stdpath("data"),
     })
-
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-    keymap.set("n", "<leader>pp", "<cmd>Telescope projects<CR>", { desc = "Fuzzy find projects" })
   end,
 }
