53b45f6f296b43c3ed90c2cff1b839d99392e43b
André Matias <andre.matias@mutant.com.br>
2024-01-29 15:10:21 -0300

    Configura lualine

--- .config/nvim/lua/matias/plugins/lualine.lua
+++ .config/nvim/lua/matias/plugins/lualine.lua
@@ -4,14 +4,49 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local lualine = require("lualine")
-
+    local colors = require("gruvbox").palette
     lualine.setup({
       options = {
+        icons_enabled = false,
         globalstatus = true,
-        section_separators = { left = "", right = "" },
-        component_separators = { left = "", right = "" },
+        component_separators = "",
+        section_separators = "",
+        -- component_separators = "|",
+        -- section_separators = { left = "", right = "" },
         disabled_filetypes = { "alpha", "dashboard" },
         always_divide_middle = false,
+      },
+      tabline = {
+        lualine_a = {
+          {
+            "buffers",
+            show_filename_only = true,
+            hide_filename_extension = true,
+            show_modified_status = true,
+            mode = 0,
+            use_mode_colors = false,
+            buffers_color = {
+              active = { fg = colors.bright_orange, bg = "none" },
+              inactive = { fg = colors.gray, bg = "none" },
+            },
+            symbols = {
+              modified = " ●",
+              alternate_file = " ",
+              directory = "",
+            },
+            filetype_names = {
+              alpha = "",
+              NvimTree = "",
+              TelescopePrompt = "",
+              aerial = "",
+            },
+          },
+        },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
       },
       sections = {
         lualine_a = {
@@ -33,35 +68,24 @@ return {
           },
         },
         lualine_b = {},
-        lualine_c = {
-          {
-            "buffers",
-            show_filename_only = true,
-            hide_filename_extension = true,
-            show_modified_status = true,
-            mode = 0,
-            use_mode_colors = true,
-            symbols = {
-              modified = " ●",
-              alternate_file = " ",
-              directory = "",
-            },
-            filetype_names = {
-              NvimTree = "",
-              TelescopePrompt = "",
-              aerial = "",
-            },
-          },
-        },
+        lualine_c = { "filename", "filesize" },
         lualine_x = {
           { "selectioncount" },
           { "searchcount" },
           { "progress" },
         },
-        lualine_y = { "tabs" },
-        lualine_z = {},
+        lualine_y = { "branch", "diagnostics" },
+        lualine_z = { "tabs" },
       },
       extensions = { "mason" },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
     })
   end,
 }
