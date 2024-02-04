091221632642c97bab7c19384e7413db660667c7
André Matias <andre.matias@mutant.com.br>
2024-02-04 15:56:35 -0300

    Utiliza o inverse no tema gruvbox

--- .config/nvim/lua/matias/plugins/colorscheme.lua
+++ .config/nvim/lua/matias/plugins/colorscheme.lua
@@ -115,7 +115,7 @@ return {
         invert_signs = false,
         invert_tabline = false,
         invert_intend_guides = false,
-        inverse = false, -- invert background for search, diffs, statuslines and errors
+        inverse = true, -- invert background for search, diffs, statuslines and errors
         contrast = "hard", -- can be "hard", "soft" or empty string
         palette_overrides = {},
         overrides = {
