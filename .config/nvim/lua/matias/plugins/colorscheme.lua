38bbbd28662354f45f488a7330c539b60c8d6448
André Matias <andrematias@Silver.local>
2025-06-16 03:40:36 -0300

    Adiciona temas do github no nvim

--- .config/nvim/lua/matias/plugins/colorscheme.lua
+++ .config/nvim/lua/matias/plugins/colorscheme.lua
@@ -175,4 +175,15 @@ return {
       -- vim.cmd.colorscheme("modus")
     end,
   },
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("github-theme").setup({})
+
+      -- vim.cmd.colorscheme("github_dark_default")
+    end,
+  },
 }
