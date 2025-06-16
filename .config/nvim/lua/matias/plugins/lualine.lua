1cfd6badcd8113d3a731b6382efc3ba15bf6d626
André Matias <andrematias@Silver.local>
2025-06-16 03:40:59 -0300

    Aplica o tema basico no lualine

--- .config/nvim/lua/matias/plugins/lualine.lua
+++ .config/nvim/lua/matias/plugins/lualine.lua
@@ -3,6 +3,7 @@ return {
   enabled = true,
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("matias.plugins.lualine.themes.evil")
+    -- require("matias.plugins.lualine.themes.evil")
+    require("matias.plugins.lualine.themes.basic")
   end,
 }
