5452a8c096c5d5f8c6610a4900d3d7bbe23922fa
André Matias <andrematias@Silver.local>
2025-06-17 14:23:20 -0300

    Define os temas que quero nos modos dark/light

--- .config/nvim/lua/matias/plugins/auto-dark-mode.lua
+++ .config/nvim/lua/matias/plugins/auto-dark-mode.lua
@@ -3,9 +3,11 @@ return {
   opts = {
     set_dark_mode = function()
       vim.api.nvim_set_option_value("background", "dark", {})
+      vim.cmd.colorscheme("vscode")
     end,
     set_light_mode = function()
       vim.api.nvim_set_option_value("background", "light", {})
+      vim.cmd.colorscheme("modus")
     end,
     update_interval = 3000,
     fallback = "dark",
