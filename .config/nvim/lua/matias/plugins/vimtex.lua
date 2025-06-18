ea0b0b623780297ac5e96706164a4f94bbbd23e0
André Matias <andrematias@Silver.local>
2025-06-18 18:13:42 -0300

    Adiciona o plugin vimtex

---
+++ .config/nvim/lua/matias/plugins/vimtex.lua
@@ -0,0 +1,26 @@
+return {
+  "lervag/vimtex",
+  init = function()
+    -- Viewer settings
+    vim.g.vimtex_view_method = "skim"
+    vim.g.vimtex_context_pdf_viewer = "skim"
+
+    -- Indentation settings
+    vim.g.vimtex_indent_enabled = false
+    vim.g.tex_indent_items = false
+    vim.g.tex_indent_brace = false
+
+    -- Suppression settings
+    vim.g.vimtex_quickfix_mode = 0
+    vim.g.vimtex_log_ignore = {
+      "Underfull",
+      "Overfull",
+      "specifier changed to",
+      "Token not allowed in a PDF string",
+    }
+
+    -- Other settings
+    vim.g.vimtex_mappings_enabled = false
+    vim.g.tex_flavor = "latex"
+  end,
+}
