c1a286de236aaef596bea34ea9383dbd5aad95b4
André Matias <dev.andrematias@gmail.com>
2025-07-02 22:43:13 -0300

    Qutwbrowser config

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -7,4 +7,17 @@ c: ConfigContainer = c  # type: ignore
 config.load_autoconfig(True)
 config.source("colors.py")
 
+# Keybindings
+config.bind("<space>ds", "open -t https://chat.deepseek.com")
+config.bind("<space>gpt", "open -t https://chat.openai.com")
+
+# Settings
 c.statusbar.show = "in-mode"
+c.tabs.show = "switching"
+
+c.url.searchengines = {
+    "DEFAULT": "https://duckduckgo.com/?q={}",
+    "gg": "https://www.google.com/search?q={}",
+    "wa": "https://wiki.archlinux.org/?search={}",
+    "yt": "https://www.youtube.com/results?search_query={}",
+}
