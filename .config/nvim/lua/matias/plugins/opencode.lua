994bd0f2cc8fa7f284182eb61379fb51a656c1d4
André Matias <dev.andrematias@gmail.com>
2026-03-07 17:37:33 -0300

    Disabilita integração do opencode com lsp do nvim

--- .config/nvim/lua/matias/plugins/opencode.lua
+++ .config/nvim/lua/matias/plugins/opencode.lua
@@ -5,19 +5,33 @@ return {
     vim.o.autoread = true
     vim.g.opencode_opts = {
       lsp = {
-        enabled = true
-      }
+        enabled = false,
+      },
     }
 
     local keymap = vim.keymap
-    keymap.set({ "n", "x" }, "<C-a>", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
-    keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
-    keymap.set({ "n", "t" }, "<C-t>", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
+    keymap.set({ "n", "x" }, "<C-a>", function()
+      require("opencode").ask("@this: ", { submit = true })
+    end, { desc = "Ask opencode…" })
+    keymap.set({ "n", "x" }, "<C-x>", function()
+      require("opencode").select()
+    end, { desc = "Execute opencode action…" })
+    keymap.set({ "n", "t" }, "<C-t>", function()
+      require("opencode").toggle()
+    end, { desc = "Toggle opencode" })
 
-    keymap.set({ "n", "x" }, "go",  function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
-    keymap.set("n",          "goo", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
+    keymap.set({ "n", "x" }, "go", function()
+      return require("opencode").operator("@this ")
+    end, { desc = "Add range to opencode", expr = true })
+    keymap.set("n", "goo", function()
+      return require("opencode").operator("@this ") .. "_"
+    end, { desc = "Add line to opencode", expr = true })
 
-    keymap.set("n", "<S-C-u>", function() require("opencode").command("session.half.page.up") end,   { desc = "Scroll opencode up" })
-    keymap.set("n", "<S-C-d>", function() require("opencode").command("session.half.page.down") end, { desc = "Scroll opencode down" })
+    keymap.set("n", "<S-C-u>", function()
+      require("opencode").command("session.half.page.up")
+    end, { desc = "Scroll opencode up" })
+    keymap.set("n", "<S-C-d>", function()
+      require("opencode").command("session.half.page.down")
+    end, { desc = "Scroll opencode down" })
   end,
 }
