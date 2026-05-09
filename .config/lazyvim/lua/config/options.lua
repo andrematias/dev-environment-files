a98af8c4de56b90138fb2cb3cb84a1d908fa957f
André Matias <dev.andrematias@gmail.com>
2026-05-09 02:09:11 -0300

    Adiciona opts do vim

--- .config/lazyvim/lua/config/options.lua
+++ .config/lazyvim/lua/config/options.lua
@@ -1,3 +1,19 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- vim cursor
+vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
+
+-- Define o limite de colunas para 80
+vim.opt.textwidth = 100
+vim.opt.colorcolumn = "100"
+
+-- Configura as opções de formatação
+-- t: quebra automática de texto usando textwidth
+-- q: permite formatar comentários com 'gq'
+-- j: remove caracteres de comentário ao unir linhas (onde apropriado)
+-- vim.opt.formatoptions:append({ "t", "q", "j" })
+
+vim.opt.wrap = true
+vim.opt.sidescrolloff = 0
